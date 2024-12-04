0f42307db25213c846bec6fb9054dfb862a4f7aa
Eduardo Yuji <yujiyyamada@gmail.com>
2024-12-04 07:59:48 -0300

    update

---
+++ Mapa_GrupoC/Mapa_GrupoC/Mapa_Tmx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="Mapa_Tmx" tilewidth="78" tileheight="58" tilecount="256" columns="16">
+ <image source="Mapa_Tmx.tmx" width="1280" height="960"/>
+</tileset>
---
+++ Mapa_GrupoC/Mapa_GrupoC/Tile_Asset_Png.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="Tile_Asset_Png" tilewidth="78" tileheight="58" tilecount="169" columns="13">
+ <image source="../Mapa_GrupoC (1)/Mapa_GrupoC/Tile_Asset_Png.png" width="1024" height="768"/>
+</tileset>
